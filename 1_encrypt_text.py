5aaef5dd07ea417eea03bc5f9c2a7488bf0dfac5
KenLZY <112811713+KenLZY@users.noreply.github.com>
2025-07-22 12:37:03 +0800

    Implement Fernet encryption and decryption for text files
    Completed the TODOs in 1_encrypt_text.py to generate a symmetric key, encrypt and decrypt text files using Fernet. Added output files for both encrypted and decrypted versions of long and short text samples.

--- 1_encrypt_text.py
+++ 1_encrypt_text.py
@@ -7,7 +7,8 @@ from cryptography.fernet import Fernet
 
 # Generate a Fernet key
 # TODO: Task 1-1
-
+symmetric_key = Fernet.generate_key()
+print(symmetric_key)
 # Prepare output destination
 try:
     shutil.rmtree("output")
@@ -22,6 +23,7 @@ def enc_text(input_filename, output_filename):
     """
     # Generate a cipher
     # TODO: Task 1-2
+    cipher = Fernet(symmetric_key)
 
     # Open the file, read as bytes
     with open(input_filename, "rb") as fp:
@@ -29,7 +31,7 @@ def enc_text(input_filename, output_filename):
 
     # Encrypt the raw bytes
     # TODO: Task 1-3
-    encrypted_bytes = None
+    encrypted_bytes = cipher.encrypt(raw_bytes)
 
     try:
         # Convert the ciphertext output to a printable string
@@ -53,6 +55,7 @@ def dec_text(input_filename, output_filename):
     """
     # Generate a cipher from key (same as above in enc_text)
     # TODO: Task 1-2
+    cipher = Fernet(symmetric_key)
 
     try:
         # Open the file containing the cyphertext, read as string
@@ -69,7 +72,7 @@ def dec_text(input_filename, output_filename):
 
     # Decrypt the cipher bytes
     # TODO: Task 1-4
-    decrypted_bytes = None
+    decrypted_bytes = cipher.decrypt(encrypted_bytes)
 
     try:
         # Convert the decrypted bytes to printable text
